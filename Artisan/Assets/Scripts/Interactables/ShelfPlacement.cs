0d82273b47d7c27bbe23ec7c9c62fd084285312a
Claire <clairethomas711@gmail.com>
2025-07-07 12:25:10 -0400

    Starting code for display shelves

---
+++ Artisan/Assets/Scripts/Interactables/ShelfPlacement.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ShelfPlacement : Interactable
+{
+    [SerializeField] List<Transform> placementLocations;
+    [SerializeField] GameObject TEMP_MODEL;
+    private string currentAcceptedItem;
+    private List<InventoryItem> shelfInventory = new List<InventoryItem>();
+    public override void Interact(InventoryItem heldItem, FarmManager farm)
+    {
+        if (heldItem.id == "")
+        {
+            if (shelfInventory.Count > 0)
+            {
+                farm.playerInventory.AddInventoryItem(shelfInventory[shelfInventory.Count - 1].id);
+                shelfInventory.Remove(shelfInventory[shelfInventory.Count - 1]);
+                if (shelfInventory.Count == 0)
+                {
+                    currentAcceptedItem = null;
+                }
+            }
+        }
+        else if (currentAcceptedItem == null)
+        {
+            currentAcceptedItem = heldItem.id;
+            shelfInventory.Add(heldItem);
+            farm.playerInventory.RemoveInventoryItem(heldItem.id);
+        }
+        else if (currentAcceptedItem == heldItem.id && shelfInventory.Count < placementLocations.Count)
+        {
+            shelfInventory.Add(heldItem);
+            farm.playerInventory.RemoveInventoryItem(heldItem.id);
+        }
+        UpdateDisplay();
+    }
+
+    void UpdateDisplay()
+    {
+        for (int i = 0; i < placementLocations.Count; i++)
+        {
+            if (placementLocations[i].childCount > 0)
+            {
+                Destroy(placementLocations[i].GetChild(0).gameObject);
+            }
+        }
+        for (int i = 0; i < shelfInventory.Count; i++)
+        {
+            Instantiate(TEMP_MODEL, placementLocations[i].position, placementLocations[i].rotation, placementLocations[i]);
+        }
+    }
+}
